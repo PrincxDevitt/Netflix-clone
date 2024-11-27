44bcb1df4f50a8877a470041bf21e8f1f5eb5649
PrincxDevitt <PrincxDevitt@gmail.com>
2024-11-27 12:19:03 -0500

    Guardar informacion correctamente

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 const TitleCards = () => {
   return (
-    <div className='titlecards'></div>
+    <div className='titlecards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card_list">
+        {cards_data.map(()=>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>card.name</p>
+          </div>
+            
+        })}
+      </div>
+    </div>
   )
 }
 
