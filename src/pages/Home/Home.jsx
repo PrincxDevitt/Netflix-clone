e40b7521225518c9a9d48d60f7e5afe0c84a3142
PrincxDevitt <PrincxDevitt@gmail.com>
2024-11-28 10:45:49 -0500

    Se creo el footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,8 @@ import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
+
 const Home = () => {
   return (
     <div className='home'>
@@ -30,6 +32,7 @@ const Home = () => {
         <TitleCards title={"Upcoming"}/>
         <TitleCards title={"Top pic for you"}/>
        </div>
+       <Footer/>
     </div>
   )
 }
